32509ea9e9d9a321d75cef534266c85854bd8b36
Stacy Kvernmo <stacy@oddbird.net>
2021-04-08 10:42:31 -0500

    add background image

--- src/js/components/dashboard/detail.tsx
+++ src/js/components/dashboard/detail.tsx
@@ -1,5 +1,14 @@
+import UserInfo from '~js/components/user/info';
 import React from 'react';
 
-const Dashboard = () => <div>Hello there!</div>;
+const Dashboard = () =>
+  <div className="slds-brand-band
+    slds-brand-band_medium
+    slds-brand-band_user
+    dashboard-header"
+  >
+    <UserInfo />
+  </div>
+;
 
 export default Dashboard;
