a1952268953fcadcb12531e317b71718498ae064
Stacy Kvernmo <stacy@oddbird.net>
2021-04-09 13:27:44 -0500

    WIP custom profile area

--- src/js/components/dashboard/detail.tsx
+++ src/js/components/dashboard/detail.tsx
@@ -2,14 +2,48 @@ import UserInfo from '~js/components/user/info';
 import React from 'react';
 
 const Dashboard = () =>
-  <div className="slds-brand-band
-    slds-brand-band_medium
-    slds-brand-band_user
-    dashboard-header"
-  >
+  <div className="dashboard-view">
+    <div className="slds-brand-band
+      slds-brand-band_medium
+      slds-brand-band_user
+      dashboard-header"
+    >
+    </div>
     {/* TODO slds-brand-band_user 404 bg image from Salesforce UX package */}
-    {/* TODO - make this larger and not a button */}
-    <UserInfo />
+    <div className="slds-grid dashboard-profile slds-p-around_large slds-m-horizontal_xx-large">
+      {/* TODO - make this larger and not a button */}
+      <UserInfo />
+      <div className="slds-grid dashboard-stats">
+        {/* TODO fake data for now */}
+        <div className="slds-grid dashboard-stat-item">
+          svg
+          <span className="dashboard-stat-highlight">
+            5
+          </span>
+          <span className="dashboard-stat-label">
+            Completed Projects
+          </span>
+        </div>
+        <div className="slds-grid dashboard-stat-item">
+          svg
+          <span className="dashboard-stat-highlight">
+            12
+          </span>
+          <span className="dashboard-stat-label">
+            Completed Tasks
+          </span>
+        </div>
+        <div className="slds-grid dashboard-stat-item">
+          svg
+          <span className="dashboard-stat-highlight">
+            20
+          </span>
+          <span className="dashboard-stat-label">
+            Project Collaborations
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 ;
 
